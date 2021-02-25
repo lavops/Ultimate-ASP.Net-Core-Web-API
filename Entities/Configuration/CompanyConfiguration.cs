48772c6088ae34e8db534001b12f7049c2647539
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-02-25 19:36:06 +0100

    Migration and Initial Data Seed

---
+++ Entities/Configuration/CompanyConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Configuration
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasData
+            (
+                new Company
+                {
+                    Id = new Guid("b3a56076-8ef3-418b-8764-931a6bd19ace"),
+                    Name = "IT_Solutions Ltd",
+                    Address = "583 Wall Dr. Gwynn Oak, MD 21207",
+                    Country = "USA"
+                },
+                new Company
+                {
+                    Id = new Guid("01a47ac2-cab8-40d8-b44e-93d02e50d605"),
+                    Name = "Admin_Solutions Ltd",
+                    Address = "313 Forest Avenue, BF 923",
+                    Country = "USA"
+                }
+            );
+        }
+    }
+}
---
+++ Entities/Configuration/EmployeeConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Configuration
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasData
+            (
+                new Employee
+                {
+                    Id = new Guid("95c117ba-3e58-4b37-a2f8-dd0b573c212d"),
+                    Name = "Sam Raiden",
+                    Age = 26,
+                    Position = "Software developer",
+                    CompanyId = new Guid("b3a56076-8ef3-418b-8764-931a6bd19ace")
+                },
+                new Employee
+                {
+                    Id = new Guid("6c780611-089a-4033-be80-83cf0998ea35"),
+                    Name = "Jana McLeaf",
+                    Age = 30,
+                    Position = "Software developer",
+                    CompanyId = new Guid("b3a56076-8ef3-418b-8764-931a6bd19ace")
+                },
+                new Employee
+                {
+                    Id = new Guid("4abf4778-4b19-4e6a-b9c5-fb970d79f10d"),
+                    Name = "Kane Miller",
+                    Age = 35,
+                    Position = "Administrator",
+                    CompanyId = new Guid("01a47ac2-cab8-40d8-b44e-93d02e50d605")
+                }
+            );
+        }
+    }
+}
