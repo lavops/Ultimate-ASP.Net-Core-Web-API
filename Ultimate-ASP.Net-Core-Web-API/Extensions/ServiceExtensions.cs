463951645785fcc7ca2b3f09f5796cf0ef4f9582
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-02-25 11:02:48 +0100

    Extension Methods and CORS Configuration

---
+++ Ultimate-ASP.Net-Core-Web-API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ultimate_ASP.Net_Core_Web_API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options => 
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+    }
+}
